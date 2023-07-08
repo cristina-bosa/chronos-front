3232c665c7cd51af7f924474f0c4e954f2df0eda
Cristina Bosa <cristinabosasanchez@gmail.com>
2023-07-08 14:39:08 +0100

    Create footer and header component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+        <p class="text-xs">Terms of Service | Privacy Policy </p>
+        <p class="text-xs">Chronos - {{ year }}</p>
+    </footer>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'FooterComponent',
+    data() {
+        const date = new Date();
+        return {
+            year: date.getFullYear(),
+        };
+    },
+};
+</script>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,5 @@
+<template>
+<header>
+    <p class="text-secundary text-xl text-extrabold">Chronos</p>
+</header>
+</template>
